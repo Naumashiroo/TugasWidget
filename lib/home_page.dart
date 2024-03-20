722da6ff9fcffe62083d972688719a8778241278
Naumashiroo <163824761+Naumashiroo@users.noreply.github.com>
2024-03-20 16:02:16 +0800

    Update Container homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,9 +20,20 @@ class HomePage extends StatelessWidget {
               'Naufal Rajwa Dwiyana Athallah 2211102441136 Pak ini SDK Flutter saya Error, kalo mau install ulang ribet... jadi seadanya saja (maaf)',
               style: TextStyle(fontSize: 16.0),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+
+            const SizedBox(height: 20.0),
+            const Text(
+              '2. Container:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
+              child: const Center(
+                child: Text(
+                  'Nau Pro Valorant',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
